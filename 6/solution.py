573bdef7bb6821a76218d74fa532beebd2a564ff
david-mcpherson <davidharrymcpherson@gmail.com>
2022-07-06 21:50:12 -0700

    modified implementation (still incorrect)

--- 6/solution.py
+++ 6/solution.py
@@ -1,17 +1,18 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         n = len(s)
-        result = ' ' * n
-        res_posn = 0
+        result = ''
         descending = True
-        
+        print("s: {s}; numRows: {numRows}; n: {n}") 
         for row in range(numRows):
             position = row
+
             while position < n:
-                result[res_posn] = s[position]
-                res_posn += 1
-                
-                if descending:
+                result += s[position]
+               
+                if position + 1 == n:
+                    break
+                elif descending:
                     position += 2 * (numRows - 1 - row)
                 else:
                     position += 2 * row
--- 6/tb.py
+++ 6/tb.py
@@ -2,6 +2,8 @@ import solution
 s = solution.Solution()
 
 assert s.convert("A", 1) == "A"
+assert s.convert("AB", 1) == "AB"
+assert s.convert("AB", 2) == "BA"
 assert s.convert("PAHNAPLSIIGYIR", 3) == "PAYPALISHIRING"
 assert s.convert("PAHNAPLSIIGYIR", 4) == "PINALSIGYAHRPI"
 
